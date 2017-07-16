af98d596dd196d292931e46f264cd2d6e00f5098
Pim Nijdam <p.nijdam@eaglescience.nl>
2017-07-16 14:55:08 +0200

    Receive notes from midi

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-name := "PianoSocrates"
+name := "EarTrainerKeyboard"
 
 version := "0.1"
 scalaVersion := "2.12.1"
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,28 +1,44 @@
 /**
   * Created by Pim Nijdam on 15/07/2017.
   */
-import javax.sound.midi._
+import javax.sound.midi.{MidiMessage, MidiSystem, Receiver, ShortMessage, Transmitter}
+
+class Checker extends Receiver {
+  private def noteName (key: Int): String = {
+    val name: List[String] = List("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+    val note = key % 12
+    val octave = key / 12 - 2
+
+    return s"${name(note)}${octave}"
+  }
+
+  override def close(): Unit = {}
+
+  override def send(message: MidiMessage, timeStamp: Long): Unit = {
+    val status = message.getStatus
+    val key = message.getMessage()(1)
+    val velocity = message.getMessage()(2)
+    val noteOn = status == 0x90 && velocity > 0
+    val noteOff = status == 0x80 || (status == 0x80 && velocity == 0)
+    if (noteOn) {
+      println(s"${noteName(key)} ")
+    }
+  }
+}
 
 object Main {
   def main(args: Array[String]) {
-    println("Hello world")
-    val info= MidiSystem.getMidiDeviceInfo
-    println("Found " + info.length + " devices")
+    // Use default transmitter and receiver
+    val receiver: Receiver = MidiSystem.getReceiver
+    val transmitter: Transmitter = MidiSystem.getTransmitter
 
-    val devices = info.map {MidiSystem.getMidiDevice}
-    val inCandidates = devices.filter(_.getMaxReceivers != 0 )
-    val outCandidates = devices.filter(_.getMaxTransmitters != 0 )
-    println("Out Candidates: ")
-    outCandidates.foreach((device) =>println(device.getDeviceInfo.getName))
-    println("In Candidates: ")
-    inCandidates.foreach((device) =>println(device.getDeviceInfo.getName))
-
-    // Play middle C on default receiver
+    // Play middle C
     val msg : ShortMessage = new ShortMessage()
-    msg.setMessage(ShortMessage.NOTE_ON, 0, 60, 93)
+    msg.setMessage(ShortMessage.NOTE_ON, 0, 60, 50)
 
-    val default = MidiSystem.getReceiver
-    println("Default output: " + MidiSystem.getReceiver)
-    default.send(msg, -1)
+    receiver.send(msg, -1)
+
+    transmitter.setReceiver(new Checker)
+    //TODO: generate sequence from transmitter, provide a checker implementation, provide a melody generator
   }
 }
