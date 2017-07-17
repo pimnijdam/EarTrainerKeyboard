f4395dc81cfd35cf61c7728ae9c4e2404067e640
Pim Nijdam <p.nijdam@eaglescience.nl>
2017-07-18 00:07:22 +0200

    Use Channel to communicate notes.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,11 @@
 /**
   * Created by Pim Nijdam on 15/07/2017.
   */
-import java.util.concurrent.LinkedBlockingQueue
 import javax.sound.midi.{MidiMessage, MidiSystem, MidiUnavailableException, Receiver, ShortMessage, Transmitter}
 
-import scala.collection.mutable.Queue
+import scala.concurrent.Channel
 
-class Note(name: String)
+class Note(val name: String)
 
 sealed trait Key
 case class MajorKey(keyNote:Note) extends Key
@@ -14,8 +13,7 @@ case class MinorKey(keyNote:Note) extends Key
 
 /** Convert midi messages to notes */
 class MidiSequencer extends Receiver {
-  val sequence: Queue[Note] = new Queue[Note]
-  val bseq = new LinkedBlockingQueue[Note]()
+  val sequence = new Channel[Note]
   private def noteName (key: Int): String = {
     val name: List[String] = List("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
     val note = key % 12
@@ -33,9 +31,7 @@ class MidiSequencer extends Receiver {
     val noteOn = status == 0x90 && velocity > 0
     if (noteOn) {
       val note = new Note(s"${noteName(key)}")
-      println(s"played: ${key}")
-      sequence += note
-      bseq.put(note)
+      sequence .write(note)
     }
   }
 }
@@ -44,14 +40,15 @@ class Teacher (receiver: Receiver, transmitter: Transmitter) {
   val sequencer: MidiSequencer = new MidiSequencer
   transmitter.setReceiver(sequencer)
 
-  for (note <- sequencer.sequence) {
-    // this doesn't work. Use some proper consumer/produces scala way
-    println(note)
-
-    // Play middle C
-    val msg : ShortMessage = new ShortMessage()
-    msg.setMessage(ShortMessage.NOTE_ON, 0, 60, 50)
-    receiver.send(msg, -1)
+  def run: Unit = {
+    while (true) {
+      val note = sequencer.sequence.read
+      println(note.name)
+      // Play middle C
+      val msg : ShortMessage = new ShortMessage()
+      msg.setMessage(ShortMessage.NOTE_ON, 0, 60, 50)
+      receiver.send(msg, -1)
+    }
   }
 }
 
@@ -61,7 +58,8 @@ object Main extends App {
     try {
       val receiver: Receiver = MidiSystem.getReceiver
       val transmitter: Transmitter = MidiSystem.getTransmitter
-      val Teacher = new Teacher(receiver, transmitter)
+      val teacher = new Teacher(receiver, transmitter)
+      teacher.run
     } catch {
       case _: MidiUnavailableException => {println("No midi keyboard found")}
       }
