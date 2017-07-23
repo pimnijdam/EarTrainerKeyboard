7a71c4b5d6ede8b6a47a18c87f29508b965c95ee
Pim Nijdam <p.nijdam@eaglescience.nl>
2017-07-23 21:38:13 +0200

    Play a note in the major scale.
    Non-fp code. Rather ugly since I don't know scala. Good enough for a first prove of concept for an actual exercise.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,8 +4,21 @@
 import javax.sound.midi.{MidiMessage, MidiSystem, MidiUnavailableException, Receiver, ShortMessage, Transmitter}
 
 import scala.concurrent.Channel
+import scala.util.Random
 
-class Note(val name: String)
+class Note(val key: Int) {
+
+  val name: String = noteName(key)
+  def keyIn = (k: Int) => (key - k) % 12
+
+  private def noteName (key: Int): String = {
+    val name: List[String] = List("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
+    val note = key % 12
+    val octave = key / 12 - 2
+
+    return s"${name(note)}${octave}"
+  }
+}
 
 sealed trait Key
 case class MajorKey(keyNote:Note) extends Key
@@ -14,13 +27,7 @@ case class MinorKey(keyNote:Note) extends Key
 /** Convert midi messages to notes */
 class MidiSequencer extends Receiver {
   val sequence = new Channel[Note]
-  private def noteName (key: Int): String = {
-    val name: List[String] = List("C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B")
-    val note = key % 12
-    val octave = key / 12 - 2
 
-    return s"${name(note)}${octave}"
-  }
 
   override def close(): Unit = {}
 
@@ -30,24 +37,51 @@ class MidiSequencer extends Receiver {
     val velocity = message.getMessage()(2)
     val noteOn = status == 0x90 && velocity > 0
     if (noteOn) {
-      val note = new Note(s"${noteName(key)}")
+      val note = new Note(key)
       sequence .write(note)
     }
   }
 }
+
+//type Melody = Seq[(Int, Note)]
+
 
 class Teacher (receiver: Receiver, transmitter: Transmitter) {
   val sequencer: MidiSequencer = new MidiSequencer
   transmitter.setReceiver(sequencer)
 
   def run: Unit = {
+    val majorKey = Seq(0,2,4,5,7,9,11,12)
+
+    val keyNote = new Note(65) //F3
+    var purposeNote = keyNote
+    var total = 0
+    var correct = 0
     while (true) {
-      val note = sequencer.sequence.read
-      println(note.name)
-      // Play middle C
+
+      Thread.sleep(1500)
       val msg : ShortMessage = new ShortMessage()
-      msg.setMessage(ShortMessage.NOTE_ON, 0, 60, 50)
+      msg.setMessage(ShortMessage.NOTE_ON, 0, purposeNote.key, 50)
       receiver.send(msg, -1)
+      Thread.sleep(500)
+      val msgOff : ShortMessage = new ShortMessage()
+      msgOff.setMessage(ShortMessage.NOTE_OFF, 0, purposeNote.key, 50)
+      receiver.send(msgOff, -1)
+
+      var noteHit = false
+      var attempts = 0
+      while (!noteHit) {
+        // Check note
+        val note = sequencer.sequence.read
+        println(note.name)
+        noteHit = note.key == purposeNote.key
+        attempts += 1
+      }
+      if (attempts == 1) correct += 1
+      total += 1
+      println(s"${correct}/${total} ~ ${correct*100/total}%")
+
+      purposeNote = new Note(keyNote.key + Random.shuffle(majorKey).head)
     }
   }
 }
